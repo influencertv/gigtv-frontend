6b353e2ee664906f4fb327ffe3e35b909dfedf97
Martin Lukasik <martilukas7@gmail.com>
2021-01-28 21:03:13 +0100

    with dacast

--- components/LiveTV/LiveTV.tsx
+++ components/LiveTV/LiveTV.tsx
@@ -1,4 +1,5 @@
 import YoutubePlayer from 'react-player/youtube'
+import Head from 'next/head'
 
 import TVAside from './TVAside'
 import * as S from './styled'
@@ -22,14 +23,34 @@ const LiveTV = () => {
 
   return (
     <>
+      <Head>
+        <script
+          src={`https://player.dacast.com/js/player.js?contentId=${data.liveTv.liveNow.videoId}`}
+        ></script>
+        <script
+          defer
+          dangerouslySetInnerHTML={{
+            __html: `
+            setTimeout(function() {
+              var CONTENT_ID = "${data.liveTv.liveNow.videoId}"
+              var myPlayer = dacast(CONTENT_ID, 'videoPlayer');
+            }, 1000)
+          `,
+          }}
+        ></script>
+      </Head>
       <S.Wrapper>
         <S.TV>
           <S.PlayerWrapper>
-            <S.Player id="videoPlayer">
-              <YoutubePlayer
-                url={`https://www.youtube.com/watch?v=${data.liveTv.liveNow.youtubeVideoId}`}
-              />
-            </S.Player>
+            {data.liveTv.liveNow.youtubeVideoId ? (
+              <S.Player id="videoPlayer">
+                <YoutubePlayer
+                  url={`https://www.youtube.com/watch?v=${data.liveTv.liveNow.youtubeVideoId}`}
+                />
+              </S.Player>
+            ) : (
+              <S.Player id="videoPlayer"></S.Player>
+            )}
           </S.PlayerWrapper>
 
           <TVAside liveNow={data.liveTv.liveNow} upNext={data.liveTv.upNext} />
